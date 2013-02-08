d297cff3f59aea4ddfb3eb7403c5d4dedcad1998
Tyler Andrews <tcandrews32@gmail.com>
2013-02-08 05:54:22 -0800

    GameRenderer Updates

--- src/com/brejral/mlbcardgame/GameRenderer.java
+++ src/com/brejral/mlbcardgame/GameRenderer.java
@@ -2,6 +2,7 @@ package com.brejral.mlbcardgame;
 
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
+import java.nio.FloatBuffer;
 
 import javax.microedition.khronos.opengles.GL10;
 
@@ -32,10 +33,60 @@ public class GameRenderer implements Renderer {
 	}
 	
 	class Background {
+		private FloatBuffer vertexBuffer;
+		private FloatBuffer textureBuffer;
+		private ByteBuffer indexBuffer;
+		
+		private int[] textures = new int[1];
+		
+		private float vertices[] = {
+				0.0f, 0.0f, 0.0f,
+				1.0f, 0.0f, 0.0f,
+				1.0f, 1.0f, 0.0f,
+				0.0f, 1.0f, 0.0f,				
+		};
+		
+		private float texture[] = {
+				0.0f, 0.0f,
+				1.0f, 0.0f,
+				1.0f, 1.0f,
+				0.0f, 1.0f,
+		};
+		
+		private byte indices[] = {
+				0,1,2,
+				0,2,3,
+		};
 		
 		public Background() {
 			ByteBuffer byteBuf = ByteBuffer.allocateDirect(vertices.length * 4);
 			byteBuf.order(ByteOrder.nativeOrder());
+			vertexBuffer = byteBuf.asFloatBuffer();
+			vertexBuffer.put(vertices);
+			vertexBuffer.position(0);
+			
+			byteBuf = ByteBuffer.allocateDirect(texture.length * 4);
+			byteBuf.order(ByteOrder.nativeOrder());
+			textureBuffer = byteBuf.asFloatBuffer();
+			textureBuffer.put(texture);
+			textureBuffer.position(0);
+			
+			indexBuffer = ByteBuffer.allocateDirect(indices.length);
+			indexBuffer.put(indices);
+			indexBuffer.position(0);
+		}
+		
+		public void draw(GL10 gl) {
+			gl.glBindTexture(GL10.GL_TEXTURE_2D, textures[0]);
+			
+			gl.glFrontFace(GL10.GL_CCW);
+			gl.glEnable(GL10.GL_CULL_FACE);
+			gl.glCullFace(GL10.GL_BACK);
+			
+			gl.glEnableClientState(GL10.GL_VERTEX_ARRAY);
+			gl.glEnableClientState(GL10.GL_TEXTURE_COORD_ARRAY);
+			
+			
 		}
 	}
 
